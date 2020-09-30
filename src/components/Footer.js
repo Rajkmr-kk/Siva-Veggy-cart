34abafedafc706cfcb8c547c4042b15bb7e8dd12
Rajkumar <rajkmrbtech@gmail.com>
2020-09-30 21:52:25 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+const Footer = props => {
+  return (
+    <footer>
+      <p className="footer-links">
+       
+        <a href="mailto:rajkmrbtech@gmail.com" target="_blank">
+          Need any help?
+        </a>
+        <span> / </span>
+        <a href="https://twitter.com/itsRk57_FC" target="_blank">
+          Say Hi on Twitter
+        </a>
+        <span> / </span>
+        <a href="https://rajkmr.netlify.app/" target="_blank">
+          Read My Blog
+        </a>
+      </p>
+      <p>
+        &copy; <a href="https://www.instagram.com/rajkmr_kk/" target="_blank">rajkmr_kk</a> <strong>Siva Veggy</strong> - Organic Store
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
