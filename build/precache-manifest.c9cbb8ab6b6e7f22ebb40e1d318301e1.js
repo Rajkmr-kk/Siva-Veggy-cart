f3b9db7c75910d0c134a1c2ee2a9f439552993d2
Rajkumar K K <69359630+Rajkmr-kk@users.noreply.github.com>
2021-02-01 12:25:27 +0530

    Add files via upload

---
+++ build/precache-manifest.c9cbb8ab6b6e7f22ebb40e1d318301e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11694f937d86e5d102f1fef5dfab89b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaa967217b82cddb3d0b",
+    "url": "/static/css/2.fc3ea6fc.chunk.css"
+  },
+  {
+    "revision": "5e823e58fcf718ba8793",
+    "url": "/static/css/main.86de387e.chunk.css"
+  },
+  {
+    "revision": "eaa967217b82cddb3d0b",
+    "url": "/static/js/2.efc60273.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.efc60273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e823e58fcf718ba8793",
+    "url": "/static/js/main.298c9ecc.chunk.js"
+  },
+  {
+    "revision": "043ae8a5b6464fe4d13f",
+    "url": "/static/js/runtime-main.c9e0369f.js"
+  }
+]);
